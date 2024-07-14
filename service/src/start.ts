b086ac80ee2a62b2b38b3b09369698b709fe31a3
spktrm <joseph.twin14@gmail.com>
2024-07-14 10:45:12 +1000

    add better logging for start.ts

--- service/src/start.ts
+++ service/src/start.ts
@@ -1,20 +1,32 @@
 import { exec } from "child_process";
 
-const runCommand = (command: string) => {
+const runCommand = (command: string, processName: string) => {
     const process = exec(command);
 
     process.stdout?.on("data", (data) => {
-        console.log(`stdout: ${data}`);
+        data.toString()
+            .split("\n")
+            .forEach((line: any) => {
+                if (line) {
+                    console.log(`${processName} stdout: ${line}`);
+                }
+            });
     });
 
     process.stderr?.on("data", (data) => {
-        console.error(`stderr: ${data}`);
+        data.toString()
+            .split("\n")
+            .forEach((line: any) => {
+                if (line) {
+                    console.error(`${processName} stderr: ${line}`);
+                }
+            });
     });
 
     process.on("close", (code) => {
-        console.log(`process exited with code ${code}`);
+        console.log(`${processName} process exited with code ${code}`);
     });
 };
 
-runCommand("npm run start-training-server");
-runCommand("npm run start-evaluation-server");
+runCommand("npm run start-training-server", "TrainingServer");
+runCommand("npm run start-evaluation-server", "EvaluationServer");
