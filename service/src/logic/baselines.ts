ddfc0f5f1204e668966e71c99725a0886324362d
spktrm <joseph.twin14@gmail.com>
2024-07-16 13:17:15 +1000

    max damage

--- service/src/logic/baselines.ts
+++ service/src/logic/baselines.ts
@@ -1,3 +1,4 @@
+import { AnyObject } from "@pkmn/sim";
 import { Action } from "../../protos/action_pb";
 import { State } from "../../protos/state_pb";
 import { StreamHandler } from "./handler";
@@ -29,8 +30,33 @@ const evalActionMapping: {
         return action;
     },
     2: ({ handler, action }) => {
-        action.setIndex(-1);
-        action.setText("default");
+        const request = handler.privatebattle.request as AnyObject;
+        const active = request.active ?? [];
+        const moves = active[0]?.moves;
+        if (moves !== undefined) {
+            const moveData: number[] = moves.map(
+                ({ id, disabled }: { id: string; disabled: boolean }) => {
+                    const trueId = id.startsWith("return") ? "return" : id;
+                    return disabled
+                        ? -100
+                        : handler.privatebattle.gens.dex.moves.get(trueId)
+                              .basePower;
+                },
+            );
+            const indexOfLargestNum = moveData.reduce(
+                (maxIndex, currentElement, currentIndex, arr) => {
+                    return currentElement > arr[maxIndex]
+                        ? currentIndex
+                        : maxIndex;
+                },
+                0,
+            );
+            action.setIndex(indexOfLargestNum);
+        } else {
+            action.setIndex(-1);
+            action.setText("default");
+        }
+
         return action;
     },
 };
