90f8e80b52e33baf514a4b3b45720def2704a323
spktrm <joseph.twin14@gmail.com>
2024-10-04 09:20:18 +1000

    fix to run on files and dirs

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,23 +5,29 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 PARENT_DIR="$(dirname "$SCRIPT_DIR")"
 
 # Define the function in a POSIX-compliant way
-run_in_dir() {
-    dir=$1
-    echo "Running function in $dir"
+run_in_dir_or_file() {
+    path=$1
+    echo "Running function on $path"
 
-    autoflake -r --in-place --remove-unused-variables --remove-all-unused-imports $dir
-    isort $dir
-    black $dir
+    if [ -d "$path" ]; then
+        autoflake -r --in-place --remove-unused-variables --remove-all-unused-imports $path
+        isort $path
+        black $path
+    elif [ -f "$path" ]; then
+        autoflake --in-place --remove-unused-variables --remove-all-unused-imports "$path"
+        isort "$path"
+        black "$path"
+    else
+        echo "$path is not a valid directory or file!"
+    fi
 }
 
-# List of directories in the parent directory (modify as needed)
-DIRS="$PARENT_DIR/heuristics $PARENT_DIR/inference $PARENT_DIR/ml $PARENT_DIR/replays $PARENT_DIR/rlenv $PARENT_DIR/viz"
+# List of directories and file patterns in the parent directory (modify as needed)
+DIRS="$PARENT_DIR/heuristics $PARENT_DIR/inference $PARENT_DIR/ml $PARENT_DIR/replays $PARENT_DIR/rlenv/*.py $PARENT_DIR/viz"
 
-# Iterate over each directory
-for DIR in $DIRS; do
-    if [ -d "$DIR" ]; then
-        run_in_dir "$DIR"
-    else
-        echo "$DIR is not a valid directory!"
-    fi
+# Iterate over each item
+for ITEM in $DIRS; do
+    for FILE_OR_DIR in $ITEM; do
+        run_in_dir_or_file "$FILE_OR_DIR"
+    done
 done
