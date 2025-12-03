fix kl prior

rl/environment/env.py
@@ -123,10 +123,10 @@ class TeamBuilderEnvironment:
         self._max_trajectory_length = max_trajectory_length
         self._min_trajectory_length = min_trajectory_length
 
-        self.species_rewards = jnp.asarray(
+        self.species_probs = jnp.asarray(
             HUMAN_SPECIES_COUNTS[generation][smogon_format.replace("_all_formats", "")]
         )
-        self.teammate_rewards = jnp.asarray(
+        self.teammate_probs = jnp.asarray(
             HUMAN_TEAMMATE_COUNTS[generation][smogon_format.replace("_all_formats", "")]
         )
 
@@ -142,8 +142,8 @@ class TeamBuilderEnvironment:
 
         self.ex = get_ex_builder_step()
 
-    def reset(self, niche_id: jax.Array) -> BuilderActorInput:
-        self.state = self._reset(niche_id)
+    def reset(self) -> BuilderActorInput:
+        self.state = self._reset()
         return self.state
 
     def step(self, agent_output: BuilderAgentOutput) -> BuilderActorInput:
@@ -157,7 +157,7 @@ class TeamBuilderEnvironment:
         return self.state
 
     @functools.partial(jax.jit, static_argnums=(0,))
-    def _reset(self, niche_id: jax.Array) -> BuilderActorInput:
+    def _reset(self) -> BuilderActorInput:
         species_mask = self.start_mask
 
         species_tokens = (
@@ -173,29 +173,31 @@ class TeamBuilderEnvironment:
                 ts=jnp.array(0, dtype=jnp.int32),
                 cum_teammate_reward=jnp.array(0, dtype=jnp.float32),
                 cum_species_reward=jnp.array(0, dtype=jnp.float32),
+                target_species_log_probs=jnp.log(
+                    (self.species_probs / self.species_probs.sum()) + 1e-8
+                ),
             ),
             history=BuilderHistoryOutput(
-                niche_id=niche_id,
                 species_tokens=species_tokens,
                 packed_set_tokens=packed_set_tokens,
             ),
         )
 
     def _calculate_species_rewards(self, species_tokens: jax.Array):
-        return jnp.take(self.species_rewards, species_tokens, axis=0).mean(
+        return jnp.take(self.species_probs, species_tokens, axis=0).mean(
             where=species_tokens != SpeciesEnum.SPECIES_ENUM___UNK
         )
 
     def _calculate_teammate_rewards(self, team_tokens: jax.Array):
         pairwise_rewards = jnp.take(
-            jnp.take(self.teammate_rewards, team_tokens, axis=0),
+            jnp.take(self.teammate_probs, team_tokens, axis=0),
             team_tokens,
             axis=1,
         )
 
         not_unk_mask = team_tokens != SpeciesEnum.SPECIES_ENUM___UNK
-        mask = jnp.outer(not_unk_mask, not_unk_mask) - jnp.eye(self._num_team_members)
-        mask = jnp.clip(mask, 0, 1)
+        mask = jnp.outer(not_unk_mask, not_unk_mask)
+        mask = jnp.clip(jnp.tril(mask), 0, 1) - jnp.eye(self._num_team_members)
 
         mask_sum = mask.sum().clip(min=1)
         teammate_reward = jnp.where(mask, pairwise_rewards, 0).sum() / mask_sum
@@ -229,6 +231,22 @@ class TeamBuilderEnvironment:
             selection_oh, packed_set_token, state.history.packed_set_tokens
         )
 
+        completed_count = (species_tokens != SpeciesEnum.SPECIES_ENUM___UNK).sum()
+        conditional = jnp.take(self.teammate_probs, species_tokens, axis=0)
+        teammate_conditionals = conditional.sum(axis=0) / completed_count.clip(min=1)
+        teammate_conditionals = jnp.where(species_mask, teammate_conditionals, 0)
+
+        # So it favours teammates more as time goes on
+        alpha = (next_ts / (state.history.species_tokens.shape[0] - 1)).clip(0, 1) ** (
+            1 / 2
+        )
+        target_species_probs = (
+            1 - alpha
+        ) * self.species_probs + alpha * teammate_conditionals
+        target_species_log_probs = jnp.log(
+            (target_species_probs / target_species_probs.sum().clip(min=1e-6)) + 1e-8
+        )
+
         return BuilderActorInput(
             env=BuilderEnvOutput(
                 species_mask=species_mask,
@@ -239,9 +257,9 @@ class TeamBuilderEnvironment:
                     state.history.species_tokens
                 ),
                 cum_species_reward=self._calculate_species_rewards(species_tokens),
+                target_species_log_probs=target_species_log_probs,
             ),
             history=BuilderHistoryOutput(
-                niche_id=state.history.niche_id,
                 species_tokens=species_tokens,
                 packed_set_tokens=packed_set_tokens,
             ),

rl/learner/learner.py
@@ -264,7 +264,6 @@ def train_step(
         builder_transitions.agent_output.actor_output.packed_set_head
     )
 
-    builder_target_discriminator_head = builder_target_pred.discriminator_head
     builder_target_value_head = builder_target_pred.value_head
     builder_target_species_head = builder_target_pred.species_head
     builder_target_packed_set_head = builder_target_pred.packed_set_head
@@ -302,24 +301,9 @@ def train_step(
         builder_valid.shape[0] :
     ]
 
-    num_niches = builder_target_discriminator_head.log_probs.shape[-1]
-    skill_reward = config.diversity_reward_scale * (
-        jnp.take_along_axis(
-            builder_target_discriminator_head.log_probs,
-            builder_history.niche_id[..., None],
-            axis=-1,
-        )
-        + jnp.log(num_niches)
-    ).squeeze(-1)
-
-    builder_phi = builder_transitions.env_output.cum_teammate_reward
-    potential_reward = config.human_prior_reward_scale * jnp.concatenate(
-        (jnp.zeros_like(builder_phi[:1]), builder_phi[1:] - builder_phi[:-1])
-    )
-
-    builder_reward = skill_reward + potential_reward
     rewards_tm1 = jnp.concatenate(
-        (builder_reward, player_transitions.env_output.win_reward), axis=0
+        (jnp.zeros_like(builder_valid), player_transitions.env_output.win_reward),
+        axis=0,
     )
 
     v_tm1 = jnp.concatenate(
@@ -461,6 +445,12 @@ def train_step(
             ),
         )
 
+    builder_prior_log_prob = jnp.take(
+        builder_transitions.env_output.target_species_log_probs,
+        builder_transitions.agent_output.actor_output.species_head.action_index,
+        axis=-1,
+    )
+
     def builder_loss_fn(params: Params):
 
         pred = promote_map(
@@ -473,7 +463,6 @@ def train_step(
         )
 
         value_head = pred.value_head
-        discriminator_head = pred.discriminator_head
         species_head = pred.species_head
         packed_set_head = pred.packed_set_head
 
@@ -484,6 +473,9 @@ def train_step(
 
         learner_actor_log_ratio = learner_log_prob - builder_actor_log_prob
         learner_actor_ratio = jnp.exp(learner_actor_log_ratio)
+
+        learner_prior_log_ratio = learner_log_prob - builder_prior_log_prob
+        learner_prior_ratio = jnp.exp(learner_prior_log_ratio)
 
         learner_target_log_ratio = learner_log_prob - builder_target_log_prob
         learner_target_ratio = jnp.exp(learner_target_log_ratio)
@@ -504,15 +496,6 @@ def train_step(
             valid=jnp.ones_like(builder_valid),
         )
 
-        loss_disc = -average(
-            jnp.take_along_axis(
-                discriminator_head.log_probs,
-                builder_history.niche_id[..., None],
-                axis=-1,
-            ).squeeze(-1),
-            jnp.ones_like(builder_valid),
-        )
-
         species_entropy = average(species_head.entropy, builder_valid)
         packed_set_entropy = average(packed_set_head.entropy, builder_valid)
         loss_entropy = -average(
@@ -529,26 +512,31 @@ def train_step(
             log_policy_ratio=learner_target_log_ratio,
             valid=builder_valid,
         )
-        loss_kl = backward_kl_loss(
+        loss_kl_rl = backward_kl_loss(
             policy_ratio=learner_actor_ratio,
             log_policy_ratio=learner_actor_log_ratio,
+            valid=builder_valid,
+        )
+        loss_kl_prior = forward_kl_loss(
+            policy_ratio=learner_prior_ratio,
+            log_policy_ratio=learner_prior_log_ratio,
             valid=builder_valid,
         )
 
         loss = (
             config.builder_policy_loss_coef * loss_pg
             + config.builder_value_loss_coef * loss_v
-            + config.builder_kl_loss_loss_coef * loss_kl
+            + config.builder_kl_loss_coef * loss_kl_rl
+            + config.builder_kl_prior_loss_coef * loss_kl_prior
             - config.builder_entropy_loss_coef * loss_entropy
-            + config.builder_discriminator_loss_coef * loss_disc
         )
 
         return loss, dict(
             builder_loss_pg=loss_pg,
             builder_loss_v=loss_v,
-            builder_loss_kl=loss_kl,
+            builder_loss_kl_rl=loss_kl_rl,
+            builder_loss_kl_prior=loss_kl_prior,
             builder_loss_entropy=loss_entropy,
-            builder_loss_discriminator=loss_disc,
             # Head entropies
             builder_species_entropy=species_entropy,
             builder_packed_set_entropy=packed_set_entropy,
@@ -602,7 +590,6 @@ def train_step(
             builder_norm_adv_std=builder_norm_advantages.std(where=builder_valid),
             builder_value_target_mean=average(builder_target_scalar_v, builder_valid),
             builder_value_target_std=builder_target_scalar_v.std(where=builder_valid),
-            builder_skill_reward_sum=skill_reward[1:].sum(axis=0).mean(),
         )
     )
 
@@ -861,25 +848,22 @@ class Learner:
                         batch,
                         self.config,
                     )
+
+                training_step = training_logs["training_step"]
+
                 if jnp.isfinite(training_logs["player_loss"]).item():
                     self.player_state = new_player_state
                 else:
-                    print(
-                        "Non-finite loss detected in player @ step",
-                        training_logs["player_steps"],
-                    )
+                    print("Non-finite loss detected in player @ step", training_step)
                     continue
 
                 if jnp.isfinite(training_logs["builder_loss"]).item():
                     self.builder_state = new_builder_state
                 else:
-                    print(
-                        "Non-finite loss detected in builder @ step",
-                        training_logs["builder_steps"],
-                    )
+                    print("Non-finite loss detected in builder @ step", training_step)
                     continue
 
-                player_step = np.array(self.player_state.step_count).item()
+                player_step = training_step.item()
 
                 if (player_step % self.config.save_interval_steps) == 0:
                     usage_logs = self.get_usage_counts(self.replay._species_counts)

rl/model/config.py
@@ -164,14 +164,12 @@ def get_builder_model_config(generation: int = 3, train: bool = False) -> Config
     base_size = 64
     num_heads = 4
     scale = 1
-    num_niches = 8
 
     entity_size = int(scale * base_size * num_heads)
 
     cfg.entity_size = entity_size
     cfg.generation = generation
     cfg.dtype = DEFAULT_DTYPE
-    cfg.num_niches = num_niches
 
     num_layers = 4
     num_heads = num_heads
@@ -202,7 +200,7 @@ def get_builder_model_config(generation: int = 3, train: bool = False) -> Config
     if generation < 4:
         cfg.encoder.need_pos = True
 
-    for name in ["species_head", "packed_set_head", "value_head", "discriminator_head"]:
+    for name in ["species_head", "packed_set_head", "value_head"]:
         head_cfg = ConfigDict()
         head_cfg.resnet = ConfigDict()
         head_cfg.resnet.num_resblocks = 1
@@ -210,10 +208,6 @@ def get_builder_model_config(generation: int = 3, train: bool = False) -> Config
 
     cfg.value_head.logits = ConfigDict()
     cfg.value_head.logits.layer_sizes = 1
-
-    cfg.discriminator_head.logits = ConfigDict()
-    cfg.discriminator_head.logits.layer_sizes = num_niches
-    cfg.discriminator_head.category_values = jnp.arange(num_niches)
 
     for head in [
         cfg.species_head,