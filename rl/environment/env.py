fix kl prior target

rl/environment/env.py
@@ -166,6 +166,9 @@ class TeamBuilderEnvironment:
         )
         packed_set_tokens = jnp.zeros_like(species_tokens)
 
+        start_target = self.species_probs * species_mask
+        start_target = start_target / (start_target.sum() + 1e-8)
+
         return BuilderActorInput(
             env=BuilderEnvOutput(
                 species_mask=species_mask,
@@ -173,9 +176,7 @@ class TeamBuilderEnvironment:
                 ts=jnp.array(0, dtype=jnp.int32),
                 cum_teammate_reward=jnp.array(0, dtype=jnp.float32),
                 cum_species_reward=jnp.array(0, dtype=jnp.float32),
-                target_species_log_probs=jnp.log(
-                    (self.species_probs / self.species_probs.sum()) + 1e-8
-                ),
+                target_species_probs=start_target,
             ),
             history=BuilderHistoryOutput(
                 species_tokens=species_tokens,
@@ -234,7 +235,6 @@ class TeamBuilderEnvironment:
         completed_count = (species_tokens != SpeciesEnum.SPECIES_ENUM___UNK).sum()
         conditional = jnp.take(self.teammate_probs, species_tokens, axis=0)
         teammate_conditionals = conditional.sum(axis=0) / completed_count.clip(min=1)
-        teammate_conditionals = jnp.where(species_mask, teammate_conditionals, 0)
 
         # So it favours teammates more as time goes on
         alpha = (next_ts / (state.history.species_tokens.shape[0] - 1)).clip(0, 1) ** (
@@ -243,8 +243,10 @@ class TeamBuilderEnvironment:
         target_species_probs = (
             1 - alpha
         ) * self.species_probs + alpha * teammate_conditionals
-        target_species_log_probs = jnp.log(
-            (target_species_probs / target_species_probs.sum().clip(min=1e-6)) + 1e-8
+
+        target_species_probs = jnp.where(species_mask, target_species_probs, 0)
+        target_species_probs = target_species_probs / (
+            target_species_probs.sum() + 1e-8
         )
 
         return BuilderActorInput(
@@ -257,7 +259,7 @@ class TeamBuilderEnvironment:
                     state.history.species_tokens
                 ),
                 cum_species_reward=self._calculate_species_rewards(species_tokens),
-                target_species_log_probs=target_species_log_probs,
+                target_species_probs=target_species_probs,
             ),
             history=BuilderHistoryOutput(
                 species_tokens=species_tokens,

rl/model/builder_model.py
@@ -402,6 +402,8 @@ def main(debug: bool = False, generation: int = 9):
             builder_actor_input.history.species_tokens > SpeciesEnum.SPECIES_ENUM___UNK
         ).item()
 
+        assert np.all(builder_actor_input.env.target_species_probs >= 0).item()
+
         for st, pst in zip(
             builder_actor_input.history.species_tokens.reshape(-1).tolist(),
             builder_actor_input.history.packed_set_tokens.reshape(-1).tolist(),
@@ -411,7 +413,7 @@ def main(debug: bool = False, generation: int = 9):
 
             print(species, packed_set)
 
-        reward_sum = builder_trajectory.env_output.target_species_log_probs.sum().item()
+        reward_sum = builder_trajectory.env_output.target_species_probs.sum().item()
         if reward_sum > max_reward:
             max_reward = reward_sum
 

rl/model/heads.py
@@ -90,7 +90,10 @@ class PolicyQKHead(nn.Module):
 
         log_prob = jnp.take(log_policy, action_index, axis=-1)
         return PolicyHeadOutput(
-            action_index=action_index, log_prob=log_prob, entropy=entropy
+            action_index=action_index,
+            log_prob=log_prob,
+            entropy=entropy,
+            log_policy=log_policy,
         )
 
 
@@ -141,7 +144,10 @@ class PolicyLogitHead(nn.Module):
 
         log_prob = jnp.take(log_policy, action_index, axis=-1)
         return PolicyHeadOutput(
-            action_index=action_index, log_prob=log_prob, entropy=entropy
+            action_index=action_index,
+            log_prob=log_prob,
+            entropy=entropy,
+            log_policy=log_policy,
         )
 
 