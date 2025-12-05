fix kl prior target

rl/environment/data.py
@@ -170,18 +170,6 @@ def make_species_count(generation: int, smogon_format: str):
     data = do_request(
         f"https://raw.githubusercontent.com/pkmn/smogon/refs/heads/main/data/stats/gen{generation}{smogon_format}.json"
     )
-    total_count = sum(d["count"] for d in data["pokemon"].values())
-    species_count = np.zeros(NUM_SPECIES, dtype=np.float32)
-    for k, v in data["pokemon"].items():
-        species_id = STOI["species"].get(toid(k), 0)
-        species_count[species_id] = v["count"] / total_count
-    return species_count
-
-
-def make_species_count(generation: int, smogon_format: str):
-    data = do_request(
-        f"https://raw.githubusercontent.com/pkmn/smogon/refs/heads/main/data/stats/gen{generation}{smogon_format}.json"
-    )
     species_count = np.zeros(NUM_SPECIES, dtype=np.float32)
     for k, v in data["pokemon"].items():
         species_id = STOI["species"][toid(k)]
@@ -199,7 +187,7 @@ def make_teammate_count(generation: int, smogon_format: str):
         for teammate, rew in v["teammates"].items():
             teammate_id = STOI["species"][toid(teammate)]
             teammate_count[species_id, teammate_id] = rew
-    return (teammate_count + teammate_count.T) / 2
+    return teammate_count
 
 
 VALID_GENERATIONS = [1, 9]

rl/learner/learner.py
@@ -445,14 +445,6 @@ def train_step(
             ),
         )
 
-    builder_prior_log_prob = (
-        builder_transitions.env_output.target_species_log_probs
-        * jax.nn.one_hot(
-            builder_transitions.agent_output.actor_output.species_head.action_index,
-            builder_transitions.env_output.target_species_log_probs.shape[-1],
-        )
-    ).sum(axis=-1)
-
     def builder_loss_fn(params: Params):
 
         pred = promote_map(
@@ -475,9 +467,6 @@ def train_step(
 
         learner_actor_log_ratio = learner_log_prob - builder_actor_log_prob
         learner_actor_ratio = jnp.exp(learner_actor_log_ratio)
-
-        learner_prior_log_ratio = learner_log_prob - builder_prior_log_prob
-        learner_prior_ratio = jnp.exp(learner_prior_log_ratio)
 
         learner_target_log_ratio = learner_log_prob - builder_target_log_prob
         learner_target_ratio = jnp.exp(learner_target_log_ratio)
@@ -519,10 +508,12 @@ def train_step(
             log_policy_ratio=learner_actor_log_ratio,
             valid=builder_valid,
         )
-        loss_kl_prior = forward_kl_loss(
-            policy_ratio=learner_prior_ratio,
-            log_policy_ratio=learner_prior_log_ratio,
-            valid=builder_valid,
+        loss_kl_prior = average(
+            optax.kl_divergence(
+                species_head.log_policy,
+                builder_transitions.env_output.target_species_probs,
+            ),
+            builder_valid,
         )
 
         loss = (