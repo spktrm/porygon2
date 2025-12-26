7bc1c49595e006d182801b83eba0d99290340a30
spktrm <joseph.twin14@gmail.com>
2025-12-26 11:08:13 +1000

    fix shapes

--- service/src/tests/ex.ts
+++ service/src/tests/ex.ts
@@ -47,16 +47,13 @@ async function playerController(player: TrainablePlayerAI, playerName: string) {
 
 async function runBattle() {
     console.log("Creating battle...");
-    const { p1, p2 } = createBattle(
-        {
-            p1Name: "Bot1",
-            p2Name: "Bot2",
-            p1team: getSampleTeam("gen9ou"),
-            p2team: getSampleTeam("gen9ou"),
-            smogonFormat: "gen9ou",
-        },
-        true,
-    );
+    const { p1, p2 } = createBattle({
+        p1Name: "Bot1",
+        p2Name: "Bot2",
+        p1team: getSampleTeam("gen9ou"),
+        p2team: getSampleTeam("gen9ou"),
+        smogonFormat: "gen9ou",
+    });
 
     console.log("Starting asynchronous player controllers...");
     let trajectories: EnvironmentTrajectory[] = [];
