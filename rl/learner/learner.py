309554fa1a32af405e3ffa01f7b605212379fb0f
spktrm <joseph.twin14@gmail.com>
2026-02-27 14:52:52 +1000

    fix learner nans

--- rl/learner/learner.py
+++ rl/learner/learner.py
@@ -13,7 +13,7 @@ import wandb.wandb_run
 from tqdm import tqdm
 
 import wandb
-from rl.environment.data import CAT_VF_SUPPORT, STOI
+from rl.environment.data import CAT_VF_SUPPORT, STOI, PackedSetFeature
 from rl.environment.interfaces import BuilderActorInput, PlayerActorInput, Trajectory
 from rl.environment.utils import clip_history, clip_packed_history, jax_segmented_cumsum
 from rl.learner.buffer import BuilderTrajectoryStore, DirectRatioLimiter, ReplayBuffer
@@ -232,7 +232,8 @@ def train_step(
     player_uniform_log_prob = jnp.log(player_uniform_prob)
 
     scale = 1 / config.gradient_accumulation_steps
-    entropy_decay = 1 / (jnp.floor((player_state.step_count + 1) * scale) ** 0.3)
+    step_adj = jnp.floor((player_state.step_count + 1) * scale)
+    entropy_decay = 1 / (jnp.maximum(step_adj, 1) ** 0.3)
 
     def player_loss_fn(params: Params):
 
@@ -396,14 +397,30 @@ def train_step(
         human_prob = jnp.concatenate(
             [human_prob_raw[1:], jnp.zeros_like(human_prob_raw[:1])], axis=0
         )
-        loss_human = -average(human_prob * learner_log_prob, builder_valid)
+        human_log_prob = jnp.log(jnp.where(human_prob > 0, human_prob, 1))
+
+        human_learner_ratio = jnp.exp(human_log_prob - learner_log_prob)
+        human_learner_log_ratio = human_log_prob - learner_log_prob
+
+        loss_human = forward_kl_loss(
+            policy_ratio=human_learner_ratio,
+            log_policy_ratio=human_learner_log_ratio,
+            valid=(
+                builder_transitions.env_output.curr_attribute
+                != PackedSetFeature.PACKED_SET_FEATURE__HIDDENPOWERTYPE
+            )
+            & (
+                builder_transitions.env_output.curr_attribute
+                != PackedSetFeature.PACKED_SET_FEATURE__GENDER
+            ),
+        )
 
         loss = (
             config.builder_policy_loss_coef * loss_pg
             + config.builder_value_loss_coef * loss_v
             + config.builder_kl_loss_coef * loss_backward_kl
             + loss_entropy
-            + config.builder_human_loss_coef * loss_human
+            + config.builder_human_loss_coef * entropy_decay * loss_human
         )
 
         return loss, dict(
