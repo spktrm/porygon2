70bd3ba938825a23d718d541d14976d656fe244f
spktrm <joseph.twin14@gmail.com>
2026-03-21 08:56:45 +1000

    increase vocab for evs

--- rl/model/config.py
+++ rl/model/config.py
@@ -25,7 +25,7 @@ DEFAULT_DTYPE = jnp.bfloat16
 def get_player_model_config(generation: int = 3, train: bool = False) -> ConfigDict:
     cfg = ConfigDict()
 
-    base_size = 96
+    base_size = 64
     num_heads = 4
     width_scale = 1
 
@@ -41,7 +41,7 @@ def get_player_model_config(generation: int = 3, train: bool = False) -> ConfigD
     cfg.encoder.dtype = DEFAULT_DTYPE
 
     # Params for scaling
-    cfg.encoder.num_latent_embeddings = 24
+    cfg.encoder.num_latent_embeddings = 16
     cfg.encoder.num_thinking_steps = 1
 
     encoder_num_layers = 1
@@ -141,7 +141,7 @@ def get_player_model_config(generation: int = 3, train: bool = False) -> ConfigD
 def get_builder_model_config(generation: int = 3, train: bool = False) -> ConfigDict:
     cfg = ConfigDict()
 
-    base_size = 96
+    base_size = 64
     num_heads = 4
     scale = 1
 
