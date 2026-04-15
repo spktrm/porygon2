6ba30fe02997991f00ea519e2507eb8a8b8b0d39
spktrm <joseph.twin14@gmail.com>
2026-04-15 11:32:12 +1000

    use neurd finally

--- rl/learner/learner.py
+++ rl/learner/learner.py
@@ -121,7 +121,6 @@ def train_step(
         * (1 - config.exploration_fraction)
         / config.exploration_fraction
     )
-    dynamic_threshold = jnp.ones_like(dynamic_threshold) * 10
 
     training_logs = {}
 
